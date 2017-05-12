6130c2c8381f73cb26f4a25ee9061e42fcc683a6
Tim Hopper <tdhopper@gmail.com>
2017-05-12 14:48:50 -0500

    fix grid filtering bug

--- static/js/custom.js
+++ static/js/custom.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
 
         var category = $(this).attr('data-filter');
 
-        $('#card-container .card').each(function() {
+        $('#card-container .grid-item').each(function() {
             if ($(this).is(category)) {
                 $(this).removeClass('filtered');
             } else {
